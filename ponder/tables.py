3a10dba04dda4c1246708cc9c5fa75147e160d6e
MZ <mz@MZs-MacBook-Pro.local>
2021-02-15 11:10:57 -0500

    add tables: Categorizers, CommitDetails, Commits, Datasets, ProblemCategories, ProblemCauses, ProblemFixes, ProblemSymptoms

--- ponder/tables.py
+++ ponder/tables.py
@@ -1,5 +1,5 @@
 import django_tables2 as tables
-from .models import Categorizations, BugFixes
+from .models import Categorizations, BugFixes, Categorizers, CommitDetails, Commits, Datasets, ProblemCategories, ProblemCauses, ProblemFixes, ProblemSymptoms
 
 class CategorizationsTable(tables.Table):
     class Meta:
@@ -10,3 +10,44 @@ class BugFixesTable(tables.Table):
     class Meta:
         model = BugFixes
         template_name = "django_tables2/bootstrap-responsive.html"
+
+class CategorizersTable(tables.Table):
+    class Meta:
+        model = Categorizers
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class CommitDetailsTable(tables.Table):
+    class Meta:
+        model = CommitDetails
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class CommitsTable(tables.Table):
+    class Meta:
+        model = Commits
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class DatasetsTable(tables.Table):
+    class Meta:
+        model = Datasets
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class ProblemCategoriesTable(tables.Table):
+    class Meta:
+        model = ProblemCategories
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class ProblemCausesTable(tables.Table):
+    class Meta:
+        model = ProblemCauses
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class ProblemFixesTable(tables.Table):
+    class Meta:
+        model = ProblemFixes
+        template_name = "django_tables2/bootstrap-responsive.html"
+
+class ProblemSymptomsTable(tables.Table):
+    class Meta:
+        model = ProblemSymptoms
+        template_name = "django_tables2/bootstrap-responsive.html"
+
--- ponder/urls.py
+++ ponder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from . import views
-from ponder.views import CategorizationsListView,BugFixesListView
+from ponder.views import CategorizationsListView,BugFixesListView, CategorizersListView, CommitDetailsListView, CommitsListView, DatasetsListView, ProblemCategoriesListView, ProblemCausesListView, ProblemFixesListView, ProblemSymptomsListView
 app_name = 'ponder'
 
 urlpatterns=[
@@ -10,7 +10,15 @@ urlpatterns=[
     url(r'^user_login/$',views.user_login,name='user_login'),
     url(r'^categorizations/$',views.categorizations,name='categorizations'),
     path("view_categorizations/", CategorizationsListView.as_view(), name='categorizations_table'),
-    path("view_bugfixes/", BugFixesListView.as_view(), name='bugfixes_table')
+    path("view_bugfixes/", BugFixesListView.as_view(), name='bugfixes_table'),
+    path("view_categorizers/", CategorizersListView.as_view(), name='categorizers_table'),
+    path("view_commitdetails/", CommitDetailsListView.as_view(), name='commitdetails_table'),
+    path("view_commits/", CommitsListView.as_view(), name='commits_table'),
+    path("view_datasets/", DatasetsListView.as_view(), name='datasets_table'),
+    path("view_problemcategories/", ProblemCategoriesListView.as_view(), name='problemcategories_table'),
+    path("view_problemcauses/", ProblemCausesListView.as_view(), name='problemcauses_table'),
+    path("view_problemfixes/", ProblemFixesListView.as_view(), name='problemfixes_table'),
+    path("view_problemsymptoms/", ProblemSymptomsListView.as_view(), name='problemsymptoms_table')
 ]
 
 '''
