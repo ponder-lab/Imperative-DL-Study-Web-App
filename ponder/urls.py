356f8847d9b643a2aa0212bcc66e562cee50f249
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2021-02-17 13:59:52 -0500

    Remove paths that all unsecure access.
    This is a temporary workaround for https://github.com/ponder-lab/Imperative-DL-Study-Web-App/issues/5 until the proper guards can be put into place.

--- ponder/urls.py
+++ ponder/urls.py
@@ -9,16 +9,16 @@ urlpatterns=[
     #url(r'^register/$',views.register,name='register'),
     url(r'^user_login/$',views.user_login,name='user_login'),
     url(r'^categorizations/$',views.categorizations,name='categorizations'),
-    path("view_categorizations/", CategorizationsListView.as_view(), name='categorizations_table'),
-    path("view_bugfixes/", BugFixesListView.as_view(), name='bugfixes_table'),
-    path("view_categorizers/", CategorizersListView.as_view(), name='categorizers_table'),
-    path("view_commitdetails/", CommitDetailsListView.as_view(), name='commitdetails_table'),
-    path("view_commits/", CommitsListView.as_view(), name='commits_table'),
-    path("view_datasets/", DatasetsListView.as_view(), name='datasets_table'),
-    path("view_problemcategories/", ProblemCategoriesListView.as_view(), name='problemcategories_table'),
-    path("view_problemcauses/", ProblemCausesListView.as_view(), name='problemcauses_table'),
-    path("view_problemfixes/", ProblemFixesListView.as_view(), name='problemfixes_table'),
-    path("view_problemsymptoms/", ProblemSymptomsListView.as_view(), name='problemsymptoms_table')
+    # path("view_categorizations/", CategorizationsListView.as_view(), name='categorizations_table'),
+    # path("view_bugfixes/", BugFixesListView.as_view(), name='bugfixes_table'),
+    # path("view_categorizers/", CategorizersListView.as_view(), name='categorizers_table'),
+    # path("view_commitdetails/", CommitDetailsListView.as_view(), name='commitdetails_table'),
+    # path("view_commits/", CommitsListView.as_view(), name='commits_table'),
+    # path("view_datasets/", DatasetsListView.as_view(), name='datasets_table'),
+    # path("view_problemcategories/", ProblemCategoriesListView.as_view(), name='problemcategories_table'),
+    # path("view_problemcauses/", ProblemCausesListView.as_view(), name='problemcauses_table'),
+    # path("view_problemfixes/", ProblemFixesListView.as_view(), name='problemfixes_table'),
+    # path("view_problemsymptoms/", ProblemSymptomsListView.as_view(), name='problemsymptoms_table')
 ]
 
 '''
