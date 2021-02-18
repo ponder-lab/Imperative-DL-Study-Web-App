6c84418b1590d9dd1dc5a22dd3946537963bb50d
Tatiana Castro Vélez <tcastrovelez@gradcenter.cuny.edu>
2021-02-18 00:41:32 -0400

    Update forms.py

--- ponder/forms.py
+++ ponder/forms.py
@@ -1,7 +1,10 @@
 #-*- coding: utf-8 -*-
 from django import forms
-from ponder.models import Categorizations, ProblemCategories, ProblemCauses, ProblemFixes, ProblemSymptoms, Commits
+from ponder.models import Categorizations, ProblemCategories, ProblemCauses, ProblemFixes, ProblemSymptoms, Commits,Categorizers
 from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+
+
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
@@ -10,21 +13,23 @@ class UserForm(forms.ModelForm):
 		fields = ('username','password','email')
 
 class CategorizationForm(forms.ModelForm):
+	rounds_options = list(Commits.objects.order_by().values_list('rounds',flat=True).distinct())
 	CHOICES = [('0', 'True'), ('1', 'False')]
 	is_func_fix = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+	rounds = forms.ChoiceField(choices=[(x, x) for x in rounds_options])
+
 	class Meta():
 		model = Categorizations
-		fields = ('sha', 'is_func_fix', 'func_fix_comment', 'problem_category', 
+		fields = ('rounds', 'sha', 'is_func_fix', 'func_fix_comment', 'problem_category', 
 			'category_comment','problem_cause','cause_comment',
 			'problem_symptom', 'symptom_comment',
 			'problem_fix', 'fix_comment', 'categorizer',
 			'should_discuss')
 		exclude = ('categorizer',)
-	def __init__(self, user=None, rounds=None, **kwargs):
-		super(CategorizationForm, self).__init__(**kwargs)
-		if user and rounds:
-			self.fields['sha'].queryset = models.Commits.objects.filter(rounds=rounds)
-			self.fields['categorizer'].queryset = models.Categorizer.objects.filter(categorizer=user)
+	def __init__(self, *args, **kwargs):
+		super(CategorizationForm, self).__init__(*args,**kwargs)
+		self.fields['sha'].queryset = Commits.objects.none()
+		#self.fields['categorizer'].queryset = Categorizers.objects.filter(categorizer=request.user)
 
 
 class ProblemCategoryForm(forms.ModelForm):
