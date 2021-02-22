282ad4991fcaf10043c4f352848183cc906b5161
Tatiana Castro Vélez <tcastrovelez@gradcenter.cuny.edu>
2021-02-22 16:24:44 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 'django.contrib.staticfiles',
 'django_tables2',
 'crispy_forms',
+'django_filters',
+'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -122,10 +123,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR,]
 
 MEDIA_ROOT = MEDIA_DIR
