d94009bf9e0e3138284a51adf8a0866d085abee3
Tatiana Castro Vélez <tcastrovelez@gradcenter.cuny.edu>
2021-02-22 16:31:57 -0400

    Update urls.py

--- ponder/urls.py
+++ ponder/urls.py
@@ -2,16 +2,17 @@ from django.urls import path,include
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from . import views
-from ponder.views import CategorizationsListView,BugFixesListView, CategorizersListView, CommitDetailsListView, CommitsListView, DatasetsListView, ProblemCategoriesListView, ProblemCausesListView, ProblemFixesListView, ProblemSymptomsListView
+#from ponder.views import CategorizationsListView,BugFixesListView, CategorizersListView, CommitDetailsListView, CommitsListView, DatasetsListView, ProblemCategoriesListView, ProblemCausesListView, ProblemFixesListView, ProblemSymptomsListView
 app_name = 'ponder'
 
 urlpatterns=[
     #url(r'^register/$',views.register,name='register'),
     url(r'^user_login/$',views.user_login,name='user_login'),
-    path('', views.CategorizationsListView.as_view(), name='categorizations_changelist'),
-    path('categorizations/add/', views.CategorizationsCreateView.as_view(), name='categorizations_add'),
+    path('categorizations/', views.CategorizationsListView.as_view(), name='categorizations_changelist'),
+    path('categorizations/add/<int:pk>', views.CategorizationsCreateView.as_view(), name='categorizations_add'),
     path("problem_detail/", views.problem_details, name='categorizations_table'),
-    path('ajax/load-shas/', views.load_shas, name='ajax_load_shas')
+    path('ajax/load-shas/', views.load_shas, name='ajax_load_shas'),
+    path("commits", views.CommitsTableView.as_view(), name='commits_table'),
     # path("view_categorizations/", CategorizationsListView.as_view(), name='categorizations_table'),
     # path("view_bugfixes/", BugFixesListView.as_view(), name='bugfixes_table'),
     # path("view_categorizers/", CategorizersListView.as_view(), name='categorizers_table'),
