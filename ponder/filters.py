ded12029909a533f1a818bcda297c32f3ca1ba07
Tatiana Castro Vélez <tcastrovelez@gradcenter.cuny.edu>
2021-03-04 12:57:31 -0400

    Update for dropdown of rounds in commits table

--- ponder/filters.py
+++ ponder/filters.py
@@ -2,6 +2,13 @@ import django_filters
 from ponder.models import Commits
 
 class RoundFilter(django_filters.FilterSet):
-    class Meta:
-        model = Commits
-        fields = ['rounds',]
+	rounds = []
+	for i in range(len(Commits.objects.values('rounds').distinct())):
+		c = Commits.objects.values('rounds').distinct()[i]
+		if(c['rounds'] != 'None'): 
+			rounds.append([c['rounds'], c['rounds']])
+
+	rounds = django_filters.ChoiceFilter(choices=rounds)
+	class Meta:
+		model = Commits
+		fields = ['rounds',]
