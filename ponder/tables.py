2eebd5883de48938a120bb9167b151a768753748
Tatiana Castro Vélez <tcastrovelez@gradcenter.cuny.edu>
2021-03-16 08:43:20 -0400

    Singular classes

--- ponder/tables.py
+++ ponder/tables.py
@@ -1,27 +1,27 @@
 import django_tables2 as tables
 from django_tables2 import TemplateColumn
-from .models import Categorizations, BugFixes, Categorizers, CommitDetails, Commits, Datasets, ProblemCategories, ProblemCauses, ProblemFixes, ProblemSymptoms
+from .models import Categorization, BugFix, Categorizer, CommitDetail, Commit, Dataset, ProblemCategory, ProblemCause, ProblemFix, ProblemSymptom
 
 class CategorizationsTable(tables.Table):
         class Meta:
-                model = Categorizations
+                model = Categorization
                 template_name = "django_tables2/bootstrap-responsive.html"
 
 class BugFixesTable(tables.Table):
         id = tables.Column(linkify=lambda record: record.get_id())
         sha = tables.Column(linkify=lambda record: record.get_sha(), attrs={"a": {"target": "_blank"}})
         class Meta:
-                model = BugFixes
+                model = BugFix
                 template_name = "django_tables2/bootstrap-responsive.html"
 
 class CategorizersTable(tables.Table):
         class Meta:
-                model = Categorizers
+                model = Categorizer
                 template_name = "django_tables2/bootstrap-responsive.html"
 
 class CommitDetailsTable(tables.Table):
 	class Meta:
-		model = CommitDetails
+		model = CommitDetail
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class CommitsTable(tables.Table):
@@ -32,7 +32,7 @@ class CommitsTable(tables.Table):
 	sha = tables.Column(linkify=lambda record: record.get_commit(), attrs={"a": {"target": "_blank"}})
 
 	class Meta:
-		model = Commits
+		model = Commit
 		exclude = ('author_email','id')
 		template_name = "django_tables2/bootstrap-responsive.html"
 		sequence = ('go_to_details','project', 'sha', 'author', 'commit_date', 'dataset', 'rounds', 'add_form')
@@ -40,19 +40,19 @@ class CommitsTable(tables.Table):
 class Categorizations_FilterTable(tables.Table):
 	sha = tables.Column(linkify=lambda record: record.get_sha(), attrs={"a": {"target": "_blank"}})
 	class Meta:
-		model = Categorizations
+		model = Categorization
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class BugFixes_FilterTable(tables.Table):
 	sha = tables.Column(linkify=lambda record: record.get_sha(), attrs={"a": {"target": "_blank"}})
 	class Meta:
-		model = Categorizations
+		model = Categorization
 		exclude = ('bug_fix_id', )
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class DatasetsTable(tables.Table):
         class Meta:
-                model = Datasets
+                model = Dataset
                 template_name = "django_tables2/bootstrap-responsive.html"
 	
 class ProblemCategoriesTable(tables.Table):
@@ -60,7 +60,7 @@ class ProblemCategoriesTable(tables.Table):
 	category = tables.Column(linkify=True)
 	description = tables.Column(linkify=True)
 	class Meta:
-		model = ProblemCategories
+		model = ProblemCategory
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class ProblemCausesTable(tables.Table):
@@ -68,20 +68,20 @@ class ProblemCausesTable(tables.Table):
 	cause = tables.Column(linkify=True)
 	description = tables.Column(linkify=True)
 	class Meta:
-		model = ProblemCauses
+		model = ProblemCause
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class ProblemFixesTable(tables.Table):
 	id = tables.Column(linkify=True)
 	fix = tables.Column(linkify=True)
 	class Meta:
-		model = ProblemFixes
+		model = ProblemFix
 		template_name = "django_tables2/bootstrap-responsive.html"
 
 class ProblemSymptomsTable(tables.Table):
 	id = tables.Column(linkify=True)
 	symptom = tables.Column(linkify=True)
 	class Meta:
-		model = ProblemSymptoms
+		model = ProblemSymptom
 		template_name = "django_tables2/bootstrap-responsive.html"
 		
