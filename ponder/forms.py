a7d54b77f17e4d4dd1d5cd7130a884ee075bd670
MZ <mz@MZs-MacBook-Pro.home>
2021-03-31 13:41:50 -0400

    Revert changes

--- ponder/forms.py
+++ ponder/forms.py
@@ -13,9 +13,8 @@ class UserForm(forms.ModelForm):
 		fields = ('username','password','email')
 		
 class CategorizationForm(forms.ModelForm):
-	CHOICES = [('0', 'False'), ('1', 'True')]
-	is_func_fix = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-	should_discuss = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, required= False)
+	is_func_fix = forms.BooleanField()
+	should_discuss = forms.BooleanField(required=False)
 	problem_category = forms.ModelChoiceField(queryset=ProblemCategory.objects.all(), required=False)
 	problem_cause = forms.ModelChoiceField(queryset=ProblemCause.objects.all(), required=False)
 	problem_fix = forms.ModelChoiceField(queryset=ProblemFix.objects.all(), required=False)
