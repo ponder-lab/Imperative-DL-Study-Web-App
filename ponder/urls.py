b05de98ec1ce956ddb860a704d40664778d3965a
MZ <mz@MZs-MacBook-Pro.lan>
2021-09-28 20:39:56 -0400

    Add roles

--- ponder/urls.py
+++ ponder/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'ponder'
 
 urlpatterns=[
+    url(r'^$',views.index,name='index'),
     url(r'^user_login/$',views.user_login,name='user_login'),
     path("commits", views.CommitsTableView.as_view(), name='commits_table'),
     path("commits/<str:pk>", views.CommitDetailsTableView.as_view(), name='commits_details'),
@@ -12,5 +13,6 @@ urlpatterns=[
     url('bug_fixes/<int:pk>', views.categorizations_by_bugFixID, name='id'),
     path('categorizations/add', views.AddCategorization, name='categorizations_add'),
     url('categorizations', views.categorizations_by_userID, name='categorizations_filter'),
-	path("success_categorization/<str:pk>", views.success_categorization, name='success_categorization')
+	path("success_categorization/<str:pk>", views.success_categorization, name='success_categorization'),
+    path('/ponder/forbidden/', views.permission_denied, name='permission_denied'),
 ]
