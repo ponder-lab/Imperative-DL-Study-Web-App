249c857e2cd6eacab575fcd952a61c5e725cd8b0
MZ <mz@MZs-MacBook-Pro.lan>
2021-10-09 17:01:22 -0400

    Add categorization update and delete operations

--- ponder/urls.py
+++ ponder/urls.py
@@ -11,8 +11,10 @@ urlpatterns=[
     path("commits/<str:pk>", views.CommitDetailsTableView.as_view(), name='commits_details'),
     path("bug_fixes/", views.BugFixesTableView.as_view(), name='bugfixes_table'),
     url('bug_fixes/<int:pk>', views.categorizations_by_bugFixID, name='id'),
+    path('categorizations/update_categorization', views.update_categorization, name='update_categorization'),
+    path('categorizations/delete_categorization', views.delete_categorization, name='delete_categorization'),
     path('categorizations/add', views.AddCategorization, name='categorizations_add'),
     url('categorizations', views.categorizations_by_userID, name='categorizations_filter'),
 	path("success_categorization/<str:pk>", views.success_categorization, name='success_categorization'),
-    path('/ponder/forbidden/', views.permission_denied, name='permission_denied'),
+    path('ponder/forbidden/', views.permission_denied, name='permission_denied'),
 ]
