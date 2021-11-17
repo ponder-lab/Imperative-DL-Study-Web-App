a4b87aea53ccf606ba2acd4b290125fc58151b2b
Zhongwei Li <zhongweili95@gmail.com>
2021-11-17 13:08:05 -0500

    ensure the user field in categorizers table is one to one

--- ponder/models.py
+++ ponder/models.py
@@ -62,7 +62,8 @@ class Categorization(models.Model):
 class Categorizer(models.Model):
 	name = models.CharField(unique=True, max_length=254)
 	initials = models.CharField(unique=True, max_length=3)
-	user = models.CharField(max_length=254)
+	#user = models.CharField(max_length=254)
+	user = models.OneToOneField(User, on_delete=models.DO_NOTHING,)
 
 	class Meta:
 		db_table = 'categorizers'
