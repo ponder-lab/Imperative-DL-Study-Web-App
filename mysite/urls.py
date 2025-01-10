fa3eeb5faf8470d1d004699be9ea198a12061f38
Y Paing <ye@y3p.io>
2025-01-10 16:34:29 -0500

    chore: prepping for cleanup by upgrading python and django to use latest

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url,include
+from django.urls import re_path, path
+from django.conf.urls import include
 from ponder import views
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
-    url(r'^special/',views.special,name='special'),
-    url(r'^',include('ponder.urls')),
-    url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^bug_fixes/', views.categorizations_by_bugFixID, name='id'),
+    re_path(r'^$',views.index,name='index'),
+    re_path(r'^special/',views.special,name='special'),
+    re_path(r'^',include('ponder.urls')),
+    re_path(r'^logout/$', views.user_logout, name='logout'),
+    re_path(r'^bug_fixes/', views.categorizations_by_bugFixID, name='id'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
