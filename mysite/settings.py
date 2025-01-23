6667f9936a05ad15dc59f43dd68e1f78e404a651
Y Paing <ye@y3p.io>
2025-01-22 20:47:51 -0500

    adding jawsdb config

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 ROOT_PATH = Path(__file__).resolve().parent.parent
@@ -90,6 +91,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # Testing with JawsDB.
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
+
 # DATABASES = {
 #     # 'default': {
 #     #     'ENGINE': 'django.db.backends.mysql',
